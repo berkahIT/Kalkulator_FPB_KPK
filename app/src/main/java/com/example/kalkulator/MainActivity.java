1b6f133211165e429413d86c8bae86c7015d1fea
auliawina <auliawinasyaf@gmail.com>
2022-01-08 11:55:25 +0700

    update

--- app/src/main/java/com/example/kalkulator/MainActivity.java
+++ app/src/main/java/com/example/kalkulator/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.kalkulator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button profile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inisialisasiView();
+    }
+    private void inisialisasiView() {
+        profile = findViewById(R.id.profile);
+        profile.setOnClickListener(view -> bukaProfileActivity());
+    }
+
+    private void bukaProfileActivity() {
+        Log.d("MAIN","Buka activity profile");
+        Intent intent = new Intent(this,Profile.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/kalkulator/Profile.java
+++ app/src/main/java/com/example/kalkulator/Profile.java
@@ -2,13 +2,26 @@ package com.example.kalkulator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 public class Profile extends AppCompatActivity {
-
+    Button menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        inisialisasView();
+    }
+    private void  inisialisasView(){
+        menu = findViewById(R.id.menu);
+        menu.setOnClickListener(view -> bukaMainActivity());
+    }
+    private void bukaMainActivity(){
+        Log.d("MAIN","Buka activity profile");
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
